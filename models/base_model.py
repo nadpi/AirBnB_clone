9b5ce38e849fb2b72fcda2af58e4e65f137b35b2
nadpi <nadawaled87@gmail.com>
2023-12-10 10:38:38 -0800

    BaseModel task

--- models/base_model.py
+++ models/base_model.py
@@ -5,35 +5,59 @@ from datetime import datetime
 
 
 class BaseModel:
-    """ class BaseModel defines all common
-        attributes/methods for other classes
-        Public instance attributes:
-        - id: string - assigned with a uuid when an instance is created.
-        - created_at: datetime - assigned with the current datetime
+    """
+    class BaseModel defines all common
+    attributes/methods for other classes
+
+    Attributes:
+        id (str): assigned with a uuid when an instance is created.
+        created_at (obj): assigned with the current datetime
         when an instance is created.
-        - updated_at: datetime - assigned with the current datetime
+        updated_at (obj): assigned with the current datetime
         when an instance is created and updated on object changes.
         """
 
     def __init__(self):
+        """
+        initialize a basemodel object.
+
+        Parameters:
+             id (str): assigned with a uuid when an instance is created.
+            created_at (obj): assigned with the current datetime
+            when an instance is created.
+            updated_at (obj): assigned with the current datetime
+            when an instance is created and updated on object changes.
+        """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """ should print: [<class name>] (<self.id>) <self.__dict__> """
+        """
+        should print: [<class name>] (<self.id>) <self.__dict__> 
+        
+        Returns:
+            str: the string to be printed
+        """
 
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """  updates the public instance attribute
-        updated_at with the current datetime """
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """  returns a dictionary containing all keys/values
-        of __dict__ of the instance """
+        """
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+
+        Returns:
+            dict: dictionary
+        """
 
         obj = self.__dict__.copy()
         obj.update({'__class__': type(self).__name__})
