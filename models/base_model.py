d8c56cc6d9cd85a355a100890750e68c113c3ef9
nadpi <nadawaled87@gmail.com>
2023-12-12 13:42:52 -0800

    BaseModel task 4

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,11 @@ class BaseModel:
             if 'updated_at' in kwargs:
                 self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if 'id' in kwargs:
-                self.id = str(uuid4())
+                self.id = kwargs['id']
         else:
-            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            self.id = str(uuid4())
 
     def __str__(self):
         """return str """
