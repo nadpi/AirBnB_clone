077ee93789fc6fb2e3749c33703afaf7ea934b18
nada <nadadwaled87@gmail.com>
2024-05-18 16:43:11 -0700

    [FileStorage Update]

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,25 +12,25 @@ class FileStorage:
 
     def all(self):
         '''returns the dictionary __objects'''
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         '''sets in __objects the obj with key <obj class name>.id'''
-        self.__objects[str(obj.__class__.__name__) + '.' + str(obj.id)] = obj
+        FileStorage.__objects[str(obj.__class__.__name__) + '.' + str(obj.id)] = obj
 
     def save(self):
         '''serializes __objects to the JSON file (path: __file_path)'''
         json_object = {}
-        for key in self.__objects:
-            json_object[key] = self.__objects[key].to_dict()
+        for key in FileStorage.__objects:
+            json_object[key] = FileStorage.__objects[key].to_dict()
 
-        with open(self.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(json_object, f)
 
     def reload(self):
         '''Deserializes the JSON file to __objects'''
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding="UTF8") as f:
+            with open(FileStorage.__file_path, 'r', encoding="UTF8") as f:
                 for key, value in json.load(f).items():
                     attri_value = eval(value["__class__"])(**value)
-                    self.__objects[key] = attri_value
+                    FileStorage.__objects[key] = attri_value
