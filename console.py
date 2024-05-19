86e0544b9a34641257f145c627f41c3d74b5bdd0
nada <nadadwaled87@gmail.com>
2024-05-18 18:07:54 -0700

    [console UPDATE]

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''console'''
 import cmd, sys
+from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,6 +18,64 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, line):
+        model = BaseModel()
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line != model.__class__.__name__:
+            print("** class doesn't exist **")
+        else:
+            model.save()
+            print(model.id)
+
+    def do_show(self, line):
+        model = BaseModel()
+        if len(line) == 0:
+            print("** class name missing **")
+            pass
+        else:
+            strlist = line.split(" ")
+            className = strlist[0]
+            classID = None
+            if len(strlist) == 2:
+                classID = strlist[1]
+            if className != model.__class__.__name__:
+                print("** class doesn't exist **")
+            elif classID == None:
+                print("** instance id missing **")
+            elif className+'.'+classID not in models.storage.all():
+                print("** no instance found ** ")
+            else:
+                dictObj = models.storage.all()
+                print(str(dictObj[className+'.'+classID]))
+
+    def do_destroy(self, line):
+        model = BaseModel()
+        if len(line) == 0:
+            print("** class name missing **")
+            pass
+        else:
+            strlist = line.split(" ")
+            className = strlist[0]
+            classID = None
+            if len(strlist) == 2:
+                classID = strlist[1]
+            if className != model.__class__.__name__:
+                print("** class doesn't exist **")
+            elif classID == None:
+                print("** instance id missing **")
+            elif className+'.'+classID not in models.storage.all():
+                print("** no instance found ** ")
+            else:
+                dictObj = models.storage.all()
+     
+    def do_all(self, line):
+        model = BaseModel()
+        if line != model.__class__.__name__:
+                print("** class doesn't exist **")
+        else:
+            print(list(models.storage.all()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
