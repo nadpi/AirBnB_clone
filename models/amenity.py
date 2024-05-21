634793e9773169fa5689ec082fdcde78a189969e
MinnaMohammed <barakatminna1@gmail.com>
2024-05-21 13:15:41 -0700

    [More classes]

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''task 9'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''Amenity Class'''
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''task 9'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''City Class'''
+    name = ""
+    state_id = ""
