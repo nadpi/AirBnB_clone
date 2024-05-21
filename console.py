bebe24235f1be021965f14dba88f140484c26c05
MinnaMohammed <barakatminna1@gmail.com>
2024-05-21 15:50:07 -0700

    [console UPDATEwq]

--- console.py
+++ console.py
@@ -16,9 +16,9 @@ class HBNBCommand(cmd.Cmd):
     '''cmd'''
     prompt = '(hbnb) '
     file = None
-    classes = {"User": "User", "BaseModel": "BaseModel", "State": "State",
-               "City": "City", "Amenity": "Amenity", "Place": "Place",
-               "Review": "Review"}
+    classes = {"User": User, "BaseModel": BaseModel, "State": State,
+               "City": City, "Amenity": Amenity, "Place": Place,
+               "Review": Review}
 
     def do_quit(self, line):
         return True
@@ -30,17 +30,16 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
-        model = BaseModel()
         if len(line) == 0:
             print("** class name missing **")
-        elif line != model.__class__.__name__:
+        elif line not in self.classes.keys():
             print("** class doesn't exist **")
         else:
+            model = self.classes[line]()
             model.save()
             print(model.id)
 
     def do_show(self, line):
-        model = BaseModel()
         if len(line) == 0:
             print("** class name missing **")
             pass
@@ -50,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
             classID = None
             if len(strlist) == 2:
                 classID = strlist[1]
-            if className != model.__class__.__name__:
+            if className not in self.classes.keys():
                 print("** class doesn't exist **")
             elif classID is None:
                 print("** instance id missing **")
@@ -60,8 +59,38 @@ class HBNBCommand(cmd.Cmd):
                 dictObj = models.storage.all()
                 print(str(dictObj[className+'.'+classID]))
 
+    def do_update(self, line):
+        if len(line) == 0:
+            print("** class name missing **")
+            pass
+        else:
+            strlist = line.split(" ")
+            className = strlist[0]
+            classID = None
+            attrName = None
+            attrValue = None
+            if len(strlist) == 1:
+                print("** instance id missing **")
+                pass
+            elif len(strlist) < 3:
+                print("** attribute name missing **")
+                pass
+            elif len(strlist) == 3:
+                print("** value missing **")
+                pass
+            elif len(strlist) >= 4:
+                classID = strlist[1]
+                attrName = strlist[2]
+                attrValue = strlist[3]
+            if className not in self.classes.keys():
+                print("** class doesn't exist **")
+            elif className+'.'+classID not in models.storage.all():
+                print("** no instance found **")
+            else:
+                model = self.classes[className]()
+                model.attrName = attrValue
+
     def do_destroy(self, line):
-        model = BaseModel()
         if len(line) == 0:
             print("** class name missing **")
             pass
@@ -71,7 +100,7 @@ class HBNBCommand(cmd.Cmd):
             classID = None
             if len(strlist) == 2:
                 classID = strlist[1]
-            if className != model.__class__.__name__:
+            if className not in self.classes.keys():
                 print("** class doesn't exist **")
             elif classID is None:
                 print("** instance id missing **")
@@ -82,8 +111,7 @@ class HBNBCommand(cmd.Cmd):
                 del dictObj[className+'.'+classID]
 
     def do_all(self, line):
-        model = BaseModel()
-        if line != model.__class__.__name__:
+        if line not in self.classes.keys():
             print("** class doesn't exist **")
         else:
             print(list(models.storage.all()))
